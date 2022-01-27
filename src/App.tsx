aa55c3ae846ad00e5d50c04d441a35f87c7e1803
Evan Anderson <eja031094@gmail.com>
2022-01-26 21:36:03 -0600

    login form outline, cleaned up project framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, Card, CardContent, CardActions, CardHeader, TextField, Typography } from '@mui/material/';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+      <Card className='login-form'>
+        <CardHeader title='Bug Tracker Login' sx={{ backgroundColor:'#3385ff', color: '#ffffff'}}>
+
+        </CardHeader>
+        <CardContent>
+          <TextField id='usernameField' label='Username' variant='outlined' sx={{ width: '100%' , marginBottom: '0.75rem'}}/>
+          <TextField id='passwordField' label='Password' variant='outlined' sx={{ width: '100%' }}/>
+        </CardContent>
+        <CardActions sx={{ justifyContent: 'flex-end' }}>
+          <Button variant='contained' size='medium' sx={{margin: '0.75rem 0.75rem'}}>Login</Button>
+        </CardActions>
+      </Card>
       </header>
     </div>
   );
